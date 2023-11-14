66cd6ab1bc58aacc9793d673021550d102d3c1bc
Enock Ngetich <ngetichenock32@gmail.com>
2023-11-14 17:25:45 +0300

    evant handling lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,24 @@
-// Code Keypad Component Here
+import React, { useState } from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const [password, setPassword] = useState("");
+
+  function handleChange(event) {
+    console.log('Entering password...');
+    setPassword(event.target.value);
+  }
+
+  return (
+    <form>
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={handleChange}
+      />
+    </form>
+  );
 }
 
 export default Keypad;
